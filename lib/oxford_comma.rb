29f7901de9bb79b5a7a2bb924271586ac7e77687
Simon Lee <simonlee0306@gmail.com>
2018-10-17 22:08:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  str = ""
 
+  if array.size == 1
+    return array[1]
+  end
+
+  array.each_index do |x|
+    if x == array.length-1
+      str << (" and " + array[x])
+    else
+      str << (", " + array[x])
+    end
+  end
+  str
 end
